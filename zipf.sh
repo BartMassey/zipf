637ece90c60a2c24c56a23dec22f8008d3acc8f0
Bart Massey <bart@cs.pdx.edu>
2010-10-28 13:57:33 -0700

    fixed shell script to meet spec

--- zipf.sh
+++ zipf.sh
@@ -13,6 +13,5 @@ sort |
 uniq -c |
 sed 's/^ *//' |
 sort -k 1rn -k 2 |
-awk '{if (f==0 || $1 < f) print ++n, $1; f = $1;}' |
-sort -k 1rn |
-awk '{print 1.0/$1, $2;}'
+awk '{print 1.0/++n, $1;}' |
+sort -k 1n
